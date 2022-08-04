8017cc6dba69a8eb4067d9cc09b186f68b324625
Vitor Moriya <vitorjmoriya@gmail.com>
2022-08-04 08:43:09 -0300

    Initial Commit

---
+++ NarutoSigns/NarutoSignsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NarutoSignsApp.swift
+//  NarutoSigns
+//
+//  Created by Vitor Moriya on 04/08/22.
+//
+
+import SwiftUI
+
+@main
+struct NarutoSignsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
